2db165c6074f5b75e9701e320b9e1c621fa45142
John Elesho <eleshojohno@gmail.com>
2020-11-02 19:24:53 +0100

    Implement a send email using mail trap

---
+++ send_mail.py
@@ -0,0 +1,22 @@
+import smtplib
+from email.mime.text import MIMEText
+
+
+def send_mail(customer, dealer, rating, comments):
+    port = 2525
+    smtp_server = 'smtp.mailtrap.io'
+    login = 'c2eca1532c0afc'
+    password = '4db32f53ce3f5b'
+    message = f"<h3>New Feedback Submission</h3><ul><li>Customer: {customer}</li><li>Dealer: {dealer}</li><li>Rating: {rating}</li><li>Comments: {comments}</li></ul>"
+
+    sender_email = 'email1@flask-feeback.com'
+    receiver_email = 'email2@flask-feedback.com'
+    msg = MIMEText(message, 'html')
+    msg['Subject'] = 'Innoson Feedback'
+    msg['From'] = sender_email
+    msg['To'] = receiver_email
+
+    # Send email
+    with smtplib.SMTP(smtp_server, port) as server:
+        server.login(login, password)
+        server.sendmail(sender_email, receiver_email, msg.as_string())
